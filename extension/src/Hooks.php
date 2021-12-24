0dea1966911fec2c8259bd1be712c147c8f0686d
lens0021 <lorentz0021@gmail.com>
2021-12-24 14:19:16 +0900

    Remove head links

--- extension/src/Hooks.php
+++ extension/src/Hooks.php
@@ -5,12 +5,25 @@ namespace MediaWiki\Extension\ThisIsNotAWiki;
 use Title;
 
 class Hooks implements
-	\MediaWiki\Hook\GetLocalURLHook
+	\MediaWiki\Hook\GetLocalURLHook,
+	\MediaWiki\Hook\OutputPageAfterGetHeadLinksArrayHook
 {
 
 	/** @inheritDoc */
 	public function onGetLocalURL( $title, &$url, $query ) {
 		$name = Title::makeName( $title->getNamespace(), $title->getDBkey() );
 		$url = "./$name.html";
+	}
+
+	/** @inheritDoc */
+	public function onOutputPageAfterGetHeadLinksArray( &$tags, $out ) {
+		foreach( [
+			'alternative-edit',
+			'opensearch',
+			'rsd',
+			'universal-edit-button',
+			] as $key ) {
+			unset( $tags[$key] );
+		}
 	}
 }
