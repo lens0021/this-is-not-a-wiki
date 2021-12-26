659211c676aadc304437ac59d4cd74538800865a
lens0021 <lorentz0021@gmail.com>
2021-12-26 09:42:46 +0900

    Hide some footer items and the toolbox

--- includes/Hooks/Hider.php
+++ includes/Hooks/Hider.php
@@ -3,11 +3,18 @@
 namespace MediaWiki\Extension\ThisIsNotAWiki\Hooks;
 
 class Hider implements
-	\MediaWiki\Hook\ParserOutputPostCacheTransformHook
+	\MediaWiki\Hook\ParserOutputPostCacheTransformHook,
+	\MediaWiki\Hook\SidebarBeforeOutputHook
 {
+	/** @inheritDoc */
 	public function onParserOutputPostCacheTransform( $parserOutput, &$text,
 		&$options
 	): void{
 		$options['enableSectionEditLinks'] = false;
+	}
+
+	/** @inheritDoc */
+	public function onSidebarBeforeOutput( $skin, &$sidebar ): void {
+		unset( $sidebar['TOOLBOX'] );
 	}
 }
